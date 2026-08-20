15c5c2357e92aa6d94cf3b314e441e10b33c60f0
Taylor Levesque <leve0311@algonquinlive.com>
2026-08-20 11:02:45 -0400

    removed nested git repo from client

---
+++ client/src/components/EditTodo.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+
+const EditTodo = ({ todo }) => {
+  const [description, setDescription] = useState(todo.description);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const body = { description };
+      const response = await fetch(
+        `http://localhost:3000/todos/${todo.todo_id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        },
+      );
+
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-warning"
+        data-toggle="modal"
+        data-target={`#id${todo.todo_id}`}
+      >
+        Edit
+      </button>
+      <div
+        className="modal"
+        id={`id${todo.todo_id}`}
+        onClick={() => setDescription(todo.description)}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Edit Todo</h4>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
+                &times;
+              </button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="todo">Todo:</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="todo"
+                    placeholder="Enter todo"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-warning"
+                onClick={handleSubmit}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditTodo;
